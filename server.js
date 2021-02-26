08d0557d5d78c0314c6b2c95d4a5307036650170
Taylor Abraham <taylor.abra8@gmail.com>
2021-02-25 19:34:34 -0500

    Fix serving of webGL

--- server.js
+++ server.js
@@ -1,15 +1,18 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const compression = require('compression');
 const path = require('path');
-const app = express();
-app.use(express.static(path.join(__dirname, '../build/')));
-// Alias /avians to /attack-of-the-avians
-app.use('/avians', express.static(path.join(__dirname, '../build/attack-of-the-avians')))
 
-app.get('/ping', (req, res) => res.send('pong'));
+const app = express();
+app.use(compression());
+app.use(express.static(path.join(__dirname, 'build/')));
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '../build', 'index.html'))
+  res.sendFile(path.join(__dirname, 'build', 'index.html'))
 });
+
+app.use('/avians', express.static(path.join(__dirname, 'build/attack-of-the-avians/')));
+
+app.get('/ping', (req, res) => res.send('pong'));
 
 app.listen(process.env.PORT || 8080);
